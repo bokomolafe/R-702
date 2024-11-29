c396102bbd5a5b901c971167ce09239adc5d75af
bokomolafe <bokomolafe@umes.edu>
2024-11-29 18:16:56 -0500

    new visualizations for airquality dataset including histogram, time-series, boxplot, and scatter plot

--- analyze_airquality.R
+++ analyze_airquality.R
@@ -58,3 +58,37 @@ ggplot(airquality_clean, aes(x = as.factor(Month), y = Ozone)) +
 cat("The average Ozone level:", mean(airquality_clean$Ozone), "\n")
 cat("The correlation between Temperature and Ozone is:", cor(airquality_clean$Temp, airquality_clean$Ozone), "\n")
 
+
+# Histogram of Ozone Levels
+ggplot(airquality_clean, aes(x = Ozone)) +
+    geom_histogram(binwidth = 10, fill = "skyblue", color = "black") +
+    labs(title = "Distribution of Ozone Levels in New York",
+         x = "Ozone (ppb)",
+         y = "Frequency")
+
+# Line plot of Temperature across different months
+ggplot(airquality_clean, aes(x = Day, y = Temp, group = Month, color = as.factor(Month))) +
+    geom_line() +
+    labs(title = "Temperature Variations Across Months",
+         x = "Day of the Month",
+         y = "Temperature (°F)",
+         color = "Month") +
+    scale_color_discrete(labels = c("May", "June", "July", "August", "September"))
+
+
+# Boxplot of Wind Speed Across Different Months
+ggplot(airquality_clean, aes(x = as.factor(Month), y = Wind)) +
+    geom_boxplot(fill = "lightgreen", color = "darkgreen") +
+    labs(title = "Wind Speed Across Different Months",
+         x = "Month",
+         y = "Wind Speed (mph)") +
+    scale_x_discrete(labels = c("May", "June", "July", "August", "September"))
+
+
+# Scatter plot for Solar Radiation vs Ozone Levels
+ggplot(airquality_clean, aes(x = Solar.R, y = Ozone)) +
+    geom_point(color = "orange") +
+    geom_smooth(method = "lm", col = "red") +
+    labs(title = "Solar Radiation vs Ozone Levels",
+         x = "Solar Radiation (lang)",
+         y = "Ozone (ppb)")
